ee4772d9c0330732b2e484bdb8565a671e2b690d
Felipe Mantilla <felipemantilla@outlook.es>
2021-03-14 03:19:26 -0500

    [fix] fix updatePrice Method from Product for correct functionallity on Mega Coverage

--- src/Models/Product.ts
+++ src/Models/Product.ts
@@ -23,25 +23,28 @@ export class Product implements IProduct {
   }
 
   updatePrice() {
-    this.sellIn--;
     switch (this.name) {
       case "Low Coverage":
+        this.sellIn--;
         this.price--;
         this.sellIn < 0 ? (this.price = 0) : null;
         break;
       case "Medium Coverage":
+        this.sellIn--;
         this.sellIn < 0 ? (this.price -= 2) : this.price--;
         this.price < 0 ? (this.price = 0) : null;
         break;
       case "Full Coverage":
+        this.sellIn--;
         this.sellIn < 0 ? (this.price += 2) : this.price++;
         this.price > 50 ? (this.price = 50) : null;
         break;
       case "Mega Coverage":
         this.price = 80;
-        this.price <= 0 ? null : (this.price = 0);
+        this.sellIn <= 0 ? null : (this.sellIn = 0);
         break;
       case "Special Full Coverage":
+        this.sellIn--;
         this.price++;
         this.sellIn < 10 ? this.price++ : null;
         this.sellIn < 5 ? this.price++ : null;
@@ -49,6 +52,7 @@ export class Product implements IProduct {
         this.price > 50 ? (this.price = 50) : null;
         break;
       case "Super Sale":
+        this.sellIn--;
         this.sellIn < 0 ? (this.price -= 4) : (this.price -= 2);
         this.price < 0 ? (this.price = 0) : null;
         break;
